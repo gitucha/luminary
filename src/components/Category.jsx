c0ebed3e7204949fcf89cf63f6ad84da86762fd7
gitucha <briantablet23@gmail.com>
2025-08-22 16:44:49 +0300

    adding category responses to user input as well as difficulty picked

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const categories = [
     {
@@ -16,6 +17,26 @@ const categories = [
 ];
 
 function Category() {
+
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedDifficulty, setSelectedDifficulty] = useState("")
+    const difficulties = ['Easy', 'Medium', 'Hard'];
+
+    function handleCategoryClick(item){
+        setSelectedCategory(item)
+        setSelectedDifficulty("")
+    }
+
+    function handleDifficultyChange(e){
+        setSelectedDifficulty(e.target.value)
+    }
+
+    function startQuiz(){
+        if(!selectedDifficulty){
+            alert("please select a difficulty")
+        }
+    }
+
     return (
         <div className="min-h-screen bg-gradient-to-br from-purple-900 to-blue-900 flex items-center justify-center p-4">
             <div className="bg-purple-800 rounded-2xl shadow-xl p-8 w-full max-w-md">
@@ -28,8 +49,9 @@ function Category() {
                                 {category.items.map((item) => (
                                     <button
                                         key={item}
-                                        className="px-4 py-2 bg-black/40 rounded-full text-white font-semibold  hover:bg-black/60 hover:focus:outline-none focus:ring-4 focus:ring-black/70
-                                        shadow-lg transition-transform duration-200 hover:scale-105"
+                                        onClick={()=> handleCategoryClick(item)}
+                                        className={`px-4 py-2 bg-black/40 rounded-full text-white font-semibold  hover:bg-black/60 hover:focus:outline-none focus:ring-4 focus:ring-black/70
+                                        shadow-lg transition-transform duration-200 hover:scale-105 ${selectedCategory === item ? "bg-purple-600 text-white" :"bg-black/40 text-white hover:bg-black/60" } `}
                                     >
                                         {item}
                                     </button>
@@ -40,6 +62,32 @@ function Category() {
 
                     ))}
                 </div>
+
+                {/* Difficulty part */}
+                {selectedCategory && (
+                    <div className="mt-6">
+                        <h3 className="text-white font-semibold mb-2">Selected:{selectedCategory}</h3>
+                        <select 
+                        value={selectedDifficulty}
+                        onChange={handleDifficultyChange}
+                        className="w-full p-2 rounded-lg bg-black/40 text-white focus:ring-2 focus:ring-purple-400"
+                        > <option value="">Select Difficulty</option> 
+                        {difficulties.map((level) =>( <option key={level} value={level} >{level}</option> ))}
+                        </select>
+                    </div>
+                )}
+
+                {/* start quiz button */}
+                {selectedCategory && (
+                    <Link to='/questioncard'>
+                    <button 
+                    onClick={startQuiz} 
+                    className="mt-6 w-full py-3 bg-gradient-to-r from-purple-600 to-blue-600 text-white font-bold rounded-lg shadow-md hover:opacity-90 transition hover:scale-105" 
+                    >Start quiz
+                    </button>
+                    </Link>
+                    
+                )}
 
             </div>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,19 +24,28 @@ function Home() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-900 to-blue-900 text-white flex flex-col items-center justify-center p-6">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Luminary</h1>
+      <h1 className="text-5xl font-extrabold mb-4">Welcome to Luminary</h1>
       <p className="text-lg text-purple-200 mb-6 text-center max-w-md">
          Step into the light of knowledge — challenge yourself with exciting quizzes, 
         discover new facts, and see how bright your mind can shine.
       </p>
 
-      
+      <div className="flex gap-4">
       <Link
         to="/category"
-        className="bg-purple-700 px-6 py-3 rounded-full hover:bg-purple-600 transition"
+        className="bg-gradient-to-r from-blue-600 to-purple-600 px-6 py-3 rounded-full hover:scale-105 transition"
       >
-        Get Started
+        Start Quiz 
       </Link>
+      <br />
+      <Link
+        to="/profile"
+        className="backdrop-blur-lg bg-gradient-to-r from-stone-700/40 to-stone-800/40 px-6 py-3 rounded-full hover:scale-105 transition"
+      >
+        To Profile
+      </Link>
+      </div>
+
     </div>
   );
 }
